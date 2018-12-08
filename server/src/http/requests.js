41616e5f22b60e71324cc3710fc759759030e11e
咸菜 <lplee7830@gmail.com>
2018-12-08 18:21:41 +0800

    fix can not get parmas from fe

--- server/src/http/requests.js
+++ server/src/http/requests.js
@@ -11,18 +11,18 @@ let requestMiddleWare = fn => {
 export default {
 
     /**
-     * function name:test api     
+     * function name:test api
      * application:测试 get 方法
      */
     async testGet(ip = '', action = '') {
         return requestMiddleWare(async () => {
-            let data = await fetchGet('/api/test/get',{data:'123'});
+            let data = await fetchGet('/api/test/get', {data: '123'});
             return data;
         });
     },
 
     /**
-     * function name:test api     
+     * function name:test api
      * application: 测试 post 方法
      */
     async testPost(name = '') {
